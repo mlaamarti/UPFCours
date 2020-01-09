9f7a27319f67521b75c6404c95e7eef01832f1ec
Hlali Haitam <haitam.hlali@gmail.com>
2020-01-09 17:29:23 +0100

    Here  you will find the routes for our application in the "Laravel way"

--- projet/routes/web.php
+++ projet/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 
@@ -31,9 +31,79 @@ Route::get('/cours', function () {
 |  Routes Admin
 |--------------------------------------------------------------------------
 */
-Route::get('/admin', function () {
-    return view('cours/login');
+
+/*---
+    ========= Login Admin
+ ---*/
+
+Route::get('/admin/', function () {
+    return view('admin/login');
 });
+
+/*---
+    ========= Home Admin
+ ---*/
+
+Route::get('/admin/home', function () {
+    return view('admin/index');
+})->name('home');
+
+/*---
+    ========= Users Admin
+ ---*/
+
+Route::get('/admin/users', function () {
+    return view('admin/users');
+})->name('users');
+
+/*---
+    ========= Category Admin
+ ---*/
+
+Route::get('/admin/category', function () {
+    return view('admin/category');
+})->name('category');
+
+
+/*---
+    ========= Cours Admin
+ ---*/
+
+Route::get('/admin/cours', function () {
+    return view('admin/cours');
+})->name('cours');
+
+/*---
+    ========= Book Admin
+ ---*/
+
+Route::get('/admin/books', function () {
+    return view('admin/books');
+})->name('books');
+
+/*---
+    ========= Script Admin
+ ---*/
+
+Route::get('/admin/scripts', function () {
+    return view('admin/scripts');
+})->name('scripts');
+
+/*---
+    ========= Profile Admin
+ ---*/
+
+Route::get('/admin/profile', function () {
+    return view('admin/profile');
+})->name('profile');
+
+/*---
+    ========= Logout Admin
+ ---*/
+
+Route::get('/admin/logout', function () {
+    return view('admin/logout');
+})->name('logout');
 
 
 /*
