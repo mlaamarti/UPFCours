1c48146f0f116825611492c500e3f80648fd6de5
hamza-gh <hamza.ghiwach.97@gmail.com>
2020-01-10 04:31:22 +0100

    create modéle : Book & Script

---
+++ projet/app/Book.php
@@ -0,0 +1,92 @@
+<?php
+
+
+namespace App;
+
+
+class Book
+{
+
+    public $name,$title,$description,$Sommaire,$auteur;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSommaire()
+    {
+        return $this->Sommaire;
+    }
+
+    /**
+     * @param mixed $Sommaire
+     */
+    public function setSommaire($Sommaire): void
+    {
+        $this->Sommaire = $Sommaire;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+
+    /**
+     * @param mixed $auteur
+     */
+    public function setAuteur($auteur): void
+    {
+        $this->auteur = $auteur;
+    }
+
+}
---
+++ projet/app/Script.php
@@ -0,0 +1,76 @@
+<?php
+
+
+namespace App;
+
+
+class Script
+{
+    public $id,$name_script,$title,$description;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNameScript()
+    {
+        return $this->name_script;
+    }
+
+    /**
+     * @param mixed $name_script
+     */
+    public function setNameScript($name_script): void
+    {
+        $this->name_script = $name_script;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+
+}
